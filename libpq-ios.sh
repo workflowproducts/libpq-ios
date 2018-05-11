7b6ffd6f042b8eb3411ecc4c72cd6605c0c13211
Nunzio Tocci <nunzio@tocci.xyz>
2018-05-11 12:43:43 -0500

    better comments

--- libpq-ios.sh
+++ libpq-ios.sh
@@ -2,34 +2,39 @@
 
 set -e
 
+# edit these version numbers to suit your needs
 VERSION=10.3
 IOS=11.3
 LIBRESSL=2.7.3
-#MAKEFLAGS=-j4
 
+# to compile libpq for iOS, we need to use the Xcode SDK by saying -isysroot on the compile line
+# there are two SDKs we need to use, the iOS device SDK (arm) and the simulator SDK (x86)
 DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer
 SDKROOT=$DEVROOT/SDKs/iPhoneOS${IOS}.sdk
 
+# check to see if the SDK version we selected exists
 if [ ! -e "$SDKROOT" ]
 then
     echo "The iOS SDK ${IOS} doesn't exist, please edit this file and fix the IOS= variable at the top."
     exit 1
 fi
 
+# download LibreSSL
 if [ ! -e "libressl-$LIBRESSL" ]
 then
     curl -OL "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL}.tar.gz"
     tar -zxf "libressl-${LIBRESSL}.tar.gz"
 fi
 
+# create a staging directory (we need this for include files later on)
 PREFIX=$(pwd)/libressl-build/
-: '
 mkdir -p $PREFIX
 mkdir -p $PREFIX/i386
 mkdir -p $PREFIX/armv7s
 mkdir -p $PREFIX/arm64
 cd libressl-${LIBRESSL}
 
+# this cleans everything out of the build directory so we can have a clean build
 if [ -e "./Makefile" ]
 then
     make distclean
@@ -43,6 +48,9 @@ DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.pla
 SDKROOT=$DEVROOT/SDKs/iPhoneSimulator${IOS}.sdk
 
 #Build iPhone Simulator library
+# --host=i386-apple-darwin sets the host platform (this tells gcc that we are cross-compiling)
+# --prefix="$PREFIX/i386" tells configure that when we run `make install` that's where we want the files to go
+# CFLAGS="... -isysroot $SDKROOT" tells gcc where the include files and libraries ares
 ./configure --host=i386-apple-darwin --prefix="$PREFIX/i386" \
   CC="/usr/bin/gcc" \
   CPPFLAGS="-I$SDKROOT/usr/include/" \
@@ -50,8 +58,9 @@ SDKROOT=$DEVROOT/SDKs/iPhoneSimulator${IOS}.sdk
   CPP="/usr/bin/cpp $CPPFLAGS" \
   LD=$DEVROOT/usr/bin/ld
 
-make -C crypto clean all install $MAKEFLAGS
-make -C ssl clean all install $MAKEFLAGS
+# only build the files we need (libcrypto, libssl, include files)
+make -C crypto clean all install
+make -C ssl clean all install
 make -C include install
 cp crypto/.libs/libcrypto.a ./libcrypto_i386.a
 cp ssl/.libs/libssl.a ./libssl_i386.a
@@ -60,6 +69,8 @@ DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/D
 SDKROOT=$DEVROOT/SDKs/iPhoneOS${IOS}.sdk
 
 #Build ARMv7s library
+# --host=arm-apple-darwin is the most important option here (relative to the above commands),
+# it tells gcc that we need arm executables isntead of intel
 ./configure --host=arm-apple-darwin --prefix="$PREFIX/armv7s" \
   CC="/usr/bin/gcc" \
   CPPFLAGS="-I$SDKROOT/usr/include/" \
@@ -67,13 +78,14 @@ SDKROOT=$DEVROOT/SDKs/iPhoneOS${IOS}.sdk
   CPP="/usr/bin/cpp -D__arm__=1 $CPPFLAGS" \
   LD=$DEVROOT/usr/bin/ld
 
-make -C crypto clean all install $MAKEFLAGS
-make -C ssl clean all install $MAKEFLAGS
+make -C crypto clean all install
+make -C ssl clean all install
 make -C include install
 cp crypto/.libs/libcrypto.a ./libcrypto_armv7s.a
 cp ssl/.libs/libssl.a ./libssl_armv7s.a
 
 #Build ARM64 library
+# arm64 is for newer phones
 ./configure --host=arm-apple-darwin --prefix="$PREFIX/arm64" \
   CC="/usr/bin/gcc" \
   CPPFLAGS="-I$SDKROOT/usr/include/" \
@@ -81,20 +93,22 @@ cp ssl/.libs/libssl.a ./libssl_armv7s.a
   CPP="/usr/bin/cpp -D__arm__=1 $CPPFLAGS" \
   LD=$DEVROOT/usr/bin/ld
 
-make -C crypto clean all install $MAKEFLAGS
-make -C ssl clean all install $MAKEFLAGS
+make -C crypto clean all install
+make -C ssl clean all install
 make -C include install
 cp crypto/.libs/libcrypto.a ./libcrypto_arm64.a
 cp ssl/.libs/libssl.a ./libssl_arm64.a
 
+# these commands take our individual static libraries and puts them into one multi-arch library
 lipo -create ./libcrypto_i386.a ./libcrypto_armv7s.a ./libcrypto_arm64.a -output ./libcrypto.a
 lipo -create ./libssl_i386.a ./libssl_armv7s.a ./libssl_arm64.a -output ./libssl.a
 
+// put them where the Xcode project will find them
 cp ./libcrypto.a ./libssl.a ../libpq-test/
 
 cd ..
-'
 
+# download postgres
 if [ ! -e "postgresql-$VERSION" ]
 then
     curl -OL "https://ftp.postgresql.org/pub/source/v${VERSION}/postgresql-${VERSION}.tar.gz"
@@ -103,6 +117,7 @@ fi
 
 cd postgresql-${VERSION}
 
+# this cleans everything out of the build directory so we can have a clean build
 if [ -e "./src/Makefile.global" ]
 then
     make -C ./src/interfaces/libpq distclean
@@ -117,6 +132,9 @@ DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.pla
 SDKROOT=$DEVROOT/SDKs/iPhoneSimulator${IOS}.sdk
 
 #Build iPhone Simulator library
+# the important differences between this and libressl above,
+# are that we are specifying where libressl is so gcc can find it (-L$PREFIX/.../lib, -I$PREFIX/.../include)
+# other than that these options are exactly the same
 ./configure --host=i386-apple-darwin --without-readline --with-openssl \
   CC="/usr/bin/gcc -L$PREFIX/i386/lib" \
   CPPFLAGS="-I$SDKROOT/usr/include/ -I$PREFIX/i386/include" \
