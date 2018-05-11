a7ccfcdb90395e00b1a00054e8cc4a0433e7f235
Nunzio Tocci <nunzio@tocci.xyz>
2018-05-11 12:33:44 -0500

    add SSL support (closes #2)

--- libpq-ios.sh
+++ libpq-ios.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-VERSION=9.6.2
+VERSION=10.3
 IOS=11.3
+LIBRESSL=2.7.3
+#MAKEFLAGS=-j4
 
 DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer
 SDKROOT=$DEVROOT/SDKs/iPhoneOS${IOS}.sdk
@@ -13,6 +15,85 @@ then
     echo "The iOS SDK ${IOS} doesn't exist, please edit this file and fix the IOS= variable at the top."
     exit 1
 fi
+
+if [ ! -e "libressl-$LIBRESSL" ]
+then
+    curl -OL "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-${LIBRESSL}.tar.gz"
+    tar -zxf "libressl-${LIBRESSL}.tar.gz"
+fi
+
+PREFIX=$(pwd)/libressl-build/
+: '
+mkdir -p $PREFIX
+mkdir -p $PREFIX/i386
+mkdir -p $PREFIX/armv7s
+mkdir -p $PREFIX/arm64
+cd libressl-${LIBRESSL}
+
+if [ -e "./Makefile" ]
+then
+    make distclean
+fi
+
+###############################
+##  iOS libssl Compilation
+###############################
+
+DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer
+SDKROOT=$DEVROOT/SDKs/iPhoneSimulator${IOS}.sdk
+
+#Build iPhone Simulator library
+./configure --host=i386-apple-darwin --prefix="$PREFIX/i386" \
+  CC="/usr/bin/gcc" \
+  CPPFLAGS="-I$SDKROOT/usr/include/" \
+  CFLAGS="$CPPFLAGS -miphoneos-version-min=${IOS} -pipe -no-cpp-precomp -isysroot $SDKROOT" \
+  CPP="/usr/bin/cpp $CPPFLAGS" \
+  LD=$DEVROOT/usr/bin/ld
+
+make -C crypto clean all install $MAKEFLAGS
+make -C ssl clean all install $MAKEFLAGS
+make -C include install
+cp crypto/.libs/libcrypto.a ./libcrypto_i386.a
+cp ssl/.libs/libssl.a ./libssl_i386.a
+
+DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer
+SDKROOT=$DEVROOT/SDKs/iPhoneOS${IOS}.sdk
+
+#Build ARMv7s library
+./configure --host=arm-apple-darwin --prefix="$PREFIX/armv7s" \
+  CC="/usr/bin/gcc" \
+  CPPFLAGS="-I$SDKROOT/usr/include/" \
+  CFLAGS="$CPPFLAGS -arch armv7s -miphoneos-version-min=${IOS} -pipe -no-cpp-precomp -isysroot $SDKROOT" \
+  CPP="/usr/bin/cpp -D__arm__=1 $CPPFLAGS" \
+  LD=$DEVROOT/usr/bin/ld
+
+make -C crypto clean all install $MAKEFLAGS
+make -C ssl clean all install $MAKEFLAGS
+make -C include install
+cp crypto/.libs/libcrypto.a ./libcrypto_armv7s.a
+cp ssl/.libs/libssl.a ./libssl_armv7s.a
+
+#Build ARM64 library
+./configure --host=arm-apple-darwin --prefix="$PREFIX/arm64" \
+  CC="/usr/bin/gcc" \
+  CPPFLAGS="-I$SDKROOT/usr/include/" \
+  CFLAGS="$CPPFLAGS -arch arm64 -miphoneos-version-min=${IOS} -pipe -no-cpp-precomp -isysroot $SDKROOT" \
+  CPP="/usr/bin/cpp -D__arm__=1 $CPPFLAGS" \
+  LD=$DEVROOT/usr/bin/ld
+
+make -C crypto clean all install $MAKEFLAGS
+make -C ssl clean all install $MAKEFLAGS
+make -C include install
+cp crypto/.libs/libcrypto.a ./libcrypto_arm64.a
+cp ssl/.libs/libssl.a ./libssl_arm64.a
+
+lipo -create ./libcrypto_i386.a ./libcrypto_armv7s.a ./libcrypto_arm64.a -output ./libcrypto.a
+lipo -create ./libssl_i386.a ./libssl_armv7s.a ./libssl_arm64.a -output ./libssl.a
+
+cp ./libcrypto.a ./libssl.a ../libpq-test/
+
+cd ..
+'
 
 if [ ! -e "postgresql-$VERSION" ]
 then
@@ -29,19 +110,19 @@ fi
 
 chmod u+x ./configure
 ###############################
-##  iOS Compilation
+##  iOS libpq Compilation
 ###############################
 
 DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer
 SDKROOT=$DEVROOT/SDKs/iPhoneSimulator${IOS}.sdk
 
 #Build iPhone Simulator library
-./configure --host=i386-apple-darwin --without-readline \
-  CC="/usr/bin/gcc" \
-  CPPFLAGS="-I$SDKROOT/usr/include/" \
+./configure --host=i386-apple-darwin --without-readline --with-openssl \
+  CC="/usr/bin/gcc -L$PREFIX/i386/lib" \
+  CPPFLAGS="-I$SDKROOT/usr/include/ -I$PREFIX/i386/include" \
   CFLAGS="$CPPFLAGS -miphoneos-version-min=${IOS} -pipe -no-cpp-precomp -isysroot $SDKROOT" \
   CPP="/usr/bin/cpp $CPPFLAGS" \
-  LD=$DEVROOT/usr/bin/ld
+  LD="$DEVROOT/usr/bin/ld -L$PREFIX/i386/lib"
 make -C src/interfaces/libpq
 cp src/interfaces/libpq/libpq.a ./libpq_i386.a
 
@@ -50,23 +131,23 @@ SDKROOT=$DEVROOT/SDKs/iPhoneOS${IOS}.sdk
 
 #Build ARMv7s library
 make -C src/interfaces/libpq distclean
-./configure --host=arm-apple-darwin --without-readline \
-  CC="/usr/bin/gcc" \
-  CPPFLAGS="-I$SDKROOT/usr/include/" \
-  CFLAGS="$CPPFLAGS -arch armv7s -pipe -no-cpp-precomp -isysroot $SDKROOT" \
+./configure --host=arm-apple-darwin --without-readline --with-openssl \
+  CC="/usr/bin/gcc -L$PREFIX/armv7s/lib" \
+  CPPFLAGS="-I$SDKROOT/usr/include/ -I$PREFIX/armv7s/include" \
+  CFLAGS="$CPPFLAGS -arch armv7s -pipe -no-cpp-precomp -isysroot $SDKROOT " \
   CPP="/usr/bin/cpp -D__arm__=1 $CPPFLAGS" \
-  LD=$DEVROOT/usr/bin/ld
+  LD="$DEVROOT/usr/bin/ld -L$PREFIX/armv7s/lib"
 make -C src/interfaces/libpq
 cp src/interfaces/libpq/libpq.a ./libpq_armv7s.a
 
 #Build ARM64 library
 make -C src/interfaces/libpq distclean
-./configure --host=arm-apple-darwin --without-readline \
-  CC="/usr/bin/gcc" \
-  CPPFLAGS="-I$SDKROOT/usr/include/" \
+./configure --host=arm-apple-darwin --without-readline --with-openssl \
+  CC="/usr/bin/gcc -L$PREFIX/arm64/lib" \
+  CPPFLAGS="-I$SDKROOT/usr/include/ -I$PREFIX/arm64/include" \
   CFLAGS="$CPPFLAGS -arch arm64 -pipe -no-cpp-precomp -isysroot $SDKROOT" \
   CPP="/usr/bin/cpp -D__arm64__=1 $CPPFLAGS" \
-  LD=$DEVROOT/usr/bin/ld
+  LD="$DEVROOT/usr/bin/ld -L$PREFIX/arm64/lib"
 make -C src/interfaces/libpq
 cp src/interfaces/libpq/libpq.a ./libpq_arm64.a
 
@@ -75,3 +156,4 @@ lipo -create ./libpq_i386.a ./libpq_armv7s.a ./libpq_arm64.a -output ./libpq.a
 
 cd ..
 cp postgresql-${VERSION}/libpq.a libpq-test/libpq.a
+
